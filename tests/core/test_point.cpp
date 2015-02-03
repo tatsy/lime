b1bdebffdab80203fb34bc903c0c498812100a74
tatsy <tatsy.mail@gmail.com>
2015-02-03 22:31:32 +0900

    Fix CMakeLists.txt

--- tests/core/test_point.cpp
+++ tests/core/test_point.cpp
@@ -4,7 +4,7 @@
 using namespace lime;
 
 const int nSimpleLoop = 100;
-lime::Random rand = lime::Random::getRNG();
+lime::Random rng = lime::Random::getRNG();
 
 TEST(Point2d, DefaultConstructor) {
 	const Point2d p;
@@ -14,8 +14,8 @@ TEST(Point2d, DefaultConstructor) {
 
 TEST(Point2d, AddAndSubtract) {
 	for (int i = 0; i < nSimpleLoop; i++) {
-		const Point2d p(rand.randReal(), rand.randReal());
-		const Point2d q(rand.randReal(), rand.randReal());
+		const Point2d p(rng.randReal(), rng.randReal());
+		const Point2d q(rng.randReal(), rng.randReal());
 		EXPECT_EQ(p.x + q.x, (p + q).x);
 		EXPECT_EQ(p.y + q.y, (p + q).y);
 		EXPECT_EQ(p.x - q.x, (p - q).x);
@@ -23,14 +23,15 @@ TEST(Point2d, AddAndSubtract) {
 	}
 }
 
-TEST_F(Point2D, MultiplyAndDiv) {
+TEST(Point2D, MultiplyAndDiv) {
 	for (int i = 0; i < nSimpleLoop; i++) {
-		const Point2d p(rand.randReal(), rand.randReal());
-		const double d = rand.randReal();
+		const Point2d p(rng.randReal(), rng.randReal());
+		const double d = rng.randReal();
 		EXPECT_EQ(p.x * d, (p * d).x);
 		EXPECT_EQ(p.y * d, (p * d).y);
 		EXPECT_EQ(p.x / d, (p / d).x);
 		EXPECT_EQ(p.y / d, (p / d).y);
+	}
 }
 
 int main(int argc, char **argv) {
