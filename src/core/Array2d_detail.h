08ec642cef214a0a65412fc3134c02f1a2baea9c
tatsy <tatsy.mail@gmail.com>
2015-02-08 20:19:55 +0900

    Add unit tests for Array2d

--- src/core/Array2d_detail.h
+++ src/core/Array2d_detail.h
@@ -28,71 +28,73 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 namespace lime {
 
-    template <class Ty>
-    Array2d<Ty>::Array2d()
-        : nrows(0)
-        , ncols(0)
-        , data(0) {
-    }
+template <class Ty>
+Array2d<Ty>::Array2d()
+    : nrows(0)
+    , ncols(0)
+    , data(0) {
+}
 
-    template <class Ty>
-    Array2d<Ty>::Array2d(int rows, int cols)
-        : nrows(rows)
-        , ncols(cols)
-        , data(0) {
-        msg_assert(rows > 0 && cols > 0, "The array size must be positive.");
-        data = new Ty[rows * cols];
-        memset(data, 0, sizeof(Ty)* rows * cols);
-    }
+template <class Ty>
+Array2d<Ty>::Array2d(int rows, int cols)
+    : nrows(rows)
+    , ncols(cols)
+    , data(0) {
+    msg_assert(rows > 0 && cols > 0, "The array size must be positive.");
+    data = new Ty[rows * cols];
+    memset(data, 0, sizeof(Ty)* rows * cols);
+}
 
-    template <class Ty>
-    Array2d<Ty>::Array2d(int rows, int cols, const Ty& value)
-        : nrows(rows)
-        , ncols(cols)
-        , data(0) {
-        msg_assert(rows > 0 && cols > 0, "The array size must be positive.");
-        data = new Ty[rows * cols];
+template <class Ty>
+Array2d<Ty>::Array2d(int rows, int cols, const Ty& value)
+    : nrows(rows)
+    , ncols(cols)
+    , data(0) {
+    msg_assert(rows > 0 && cols > 0, "The array size must be positive.");
+    data = new Ty[rows * cols];
 
-        Ty *s = &data[0];
-        Ty *e = &data[rows * cols - 1];
-        for (Ty* p = s; p != e; ++p) *p = value;
-    }
+    Ty *s = &data[0];
+    Ty *e = &data[rows * cols - 1];
+    for (Ty* p = s; p != e; ++p) *p = value;
+}
 
-    template <class Ty>
-    Array2d<Ty>::Array2d(const Array2d<Ty>& ary)
-        : nrows(ary.nrows)
-        , ncols(ary.ncols)
-        , data(0) {
-        data = new Ty[ary.nrows * ary.ncols];
-        memcpy(data, ary.data, sizeof(Ty)* ary.nrows * ary.ncols);
-    }
+template <class Ty>
+Array2d<Ty>::Array2d(const Array2d<Ty>& ary)
+    : nrows(ary.nrows)
+    , ncols(ary.ncols)
+    , data(0) {
+    data = new Ty[ary.nrows * ary.ncols];
+    memcpy(data, ary.data, sizeof(Ty)* ary.nrows * ary.ncols);
+}
 
-    template <class Ty>
-    Array2d<Ty>& Array2d<Ty>::operator=(const Array2d<Ty>& ary) {
-        this->nrows = ary.nrows;
-        this->ncols = ary.ncols;
+template <class Ty>
+Array2d<Ty>& Array2d<Ty>::operator=(const Array2d<Ty>& ary) {
+    this->nrows = ary.nrows;
+    this->ncols = ary.ncols;
 
-        delete[] data;
-        data = new Ty[ary.nrows * ary.ncols];
-        memcpy(data, ary.data, sizeof(Ty)* ary.nrows * ary.ncols);
+    delete[] data;
+    data = new Ty[ary.nrows * ary.ncols];
+    memcpy(data, ary.data, sizeof(Ty)* ary.nrows * ary.ncols);
 
-        return *this;
-    }
+    return *this;
+}
 
-    template <class Ty>
-    Ty& Array2d<Ty>::operator()(int i, int j) const {
-        return data[i * ncols + j];
-    }
+template <class Ty>
+Ty& Array2d<Ty>::operator()(int i, int j) const {
+    msg_assert(i >= 0 && j >= 0 && i < nrows && j < ncols, "Array index out of bounds");
+    return data[i * ncols + j];
+}
 
-    template <class Ty>
-    int Array2d<Ty>::rows() const {
-        return nrows;
-    }
+template <class Ty>
+int Array2d<Ty>::rows() const {
+    return nrows;
+}
 
-    template <class Ty>
-    int Array2d<Ty>::cols() const {
-        return ncols;
-    }
+template <class Ty>
+int Array2d<Ty>::cols() const {
+    return ncols;
+}
+
 }  // namespace lime
 
 #endif  // SRC_CORE_ARRAY2D_DETAIL_H_
--- tests/core/test_array2d.cpp
+++ tests/core/test_array2d.cpp
@@ -22,38 +22,54 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "gtest/gtest.h"
 
 #include "../../include/lime.hpp"
-using lime::random_queue;
+using lime::Array2d;
 
-static const int nTest = 1 << 20;
-
-class RandomQueueTest : public ::testing::Test {
+class Array2dTest : public ::testing::Test {
  protected:
     virtual void setUp() {
     }
 
-    random_queue<int> que;
+    Array2d<int> array2d;
 };
 
-TEST_F(RandomQueueTest, InEmpty) {
-    EXPECT_TRUE(que.empty());
+TEST_F(Array2dTest, Array2dSizeTest) {
+    EXPECT_EQ(array2d.rows(), 0);
+    EXPECT_EQ(array2d.cols(), 0);
 }
 
-TEST_F(RandomQueueTest, EnqueueDequeueWorks) {
-    for (int i = 0; i < nTest; i++) {
-        EXPECT_EQ(que.size(), i);
-        que.push(i);
-    }
-    EXPECT_FALSE(que.empty());
-    for (int i = 0; i < nTest; i++) {
-        EXPECT_EQ(que.size(), nTest - i);
-        que.pop();
-    }
-    EXPECT_TRUE(que.empty());
+TEST_F(Array2dTest, NormalOperation) {
+    array2d = Array2d<int>(100, 100);
+    EXPECT_EQ(array2d.rows(), 100);
+    EXPECT_EQ(array2d.cols(), 100);
+    EXPECT_EQ(array2d(0, 0), 0);
+
+    ASSERT_DEATH(array2d(100, 0), "");
+    ASSERT_DEATH(array2d(0, 100), "");
+    ASSERT_DEATH(array2d(100, 100), "");
+    ASSERT_DEATH(array2d(-1, -1), "");
 }
 
-TEST_F(RandomQueueTest, InvalidOperation) {
-    ASSERT_DEATH(que.pop(), "");
+TEST_F(Array2dTest, CopyOperation) {
+    array2d = Array2d<int>(10, 10);
+    array2d(5, 5) = 25;
+    Array2d<int> temp = array2d;
+    EXPECT_EQ(temp(0, 0), 0);
+    EXPECT_EQ(temp(5, 5), 25);
+    temp(5, 5) = 0;
+    EXPECT_NE(array2d(5, 5), 0);
 }
+
+TEST_F(Array2dTest, OperatorEq) {
+    array2d = Array2d<int>(10, 10);
+    array2d(5, 5) = 25;
+    Array2d<int> temp;
+    temp = array2d;
+    EXPECT_EQ(temp(0, 0), 0);
+    EXPECT_EQ(temp(5, 5), 25);
+    temp(5, 5) = 0;
+    EXPECT_NE(array2d(5, 5), 0);
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
