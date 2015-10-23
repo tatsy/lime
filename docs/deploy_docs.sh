3bc552e7e1e7773aa943117ccaaa04f027efe85f
tatsy <tatsy.mail@gmail.com>
2015-10-24 01:53:39 +0900

    Update docs deployment script.

--- docs/deploy_docs.sh
+++ docs/deploy_docs.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set +x
 doxygen doxyfile
-git branch -b gh-pages
+git checkout -b gh-pages
 git push --quiet https://$GH_TOKEN@github.com/tatsy/lime.git gh-pages:gh-pages 2> /dev/null
+set -x
