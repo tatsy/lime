72093e7df7f096325891144380dea3699ef236ae
tatsy <tatsy.mail@gmail.com>
2015-10-24 02:18:36 +0900

    Update docs deployment script.

--- docs/deploy_docs.sh
+++ docs/deploy_docs.sh
@@ -2,7 +2,7 @@
 set -x
 doxygen doxyfile
 git checkout -b gh-pages
-git add --all .
+git add --all
 git commit -m "Documentation updated from Travis CI, Build No. $TRAVIS_BUILD_NUMBER."
 git push --quiet https://$GH_TOKEN@github.com/tatsy/lime.git gh-pages:gh-pages 2> /dev/null
 set +x
