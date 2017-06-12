aeca47f0faf987a1b93cd6b32d9b801646fe5901
tatsy <tatsy.mail@gmail.com>
2017-06-12 10:41:18 +0900

    Minor update.

--- docs/deploy_docs.sh
+++ docs/deploy_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-set -x
-doxygen doxyfile && make html
+doxygen doxyfile
+make html
 rm -rf html/*
 cp --recursive build/html/* html
 cd html
@@ -8,4 +8,3 @@ git checkout -b gh-pages
 git add --all .
 git commit -m "Documentation updated from Travis CI, Build No. $TRAVIS_BUILD_NUMBER."
 git push --quiet --force https://$GH_TOKEN@github.com/tatsy/lime.git gh-pages:gh-pages 2> /dev/null
-set +x
