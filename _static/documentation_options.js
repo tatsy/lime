bd1783dc60a444ffeea6eb8789e257fe2435ee1d
Travis CI <travis@travis-ci.org>
2019-10-03 07:51:06 +0000

    Documentation updated from Travis CI, Build No. 256.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,18 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-<<<<<<< HEAD
-    VERSION: '0.3.0',
-=======
-    VERSION: '0.4.0',
->>>>>>> 6cebc6323338b1e3cff8fcc4984f3603c61b3524
+    VERSION: '0.5.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
-<<<<<<< HEAD
-    SOURCELINK_SUFFIX: '.txt'
-=======
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
->>>>>>> 6cebc6323338b1e3cff8fcc4984f3603c61b3524
 };
